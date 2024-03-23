997e5c43abe3e690696ee527ceff50446129f471
BambooSword <herodreamdeng@gmail.com>
2024-03-24 00:25:35 +0800

    Add App.js and import it in main.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from './core/react.js'
+const App = React.createElement(
+  'div',
+  { id: 'root' },
+  'Hello, World!!!',
+  '你好，世界'
+)
+console.log('🚀 ~ App:', App)
+
+export default App
--- main.js
+++ main.js
@@ -1,33 +1,4 @@
 import ReactDom from './core/ReactDom.js'
-import React from './core/react.js'
-const textEl = {
-  children: [],
-  nodeValue: 'Hello, World!!!',
-}
-
-const el = {
-  type: 'div',
-  props: {
-    id: 'app',
-  },
-  children: [textEl],
-}
-
-// const dom = document.createElement(App.type)
-// dom.id = App.props.id
-// document.getElementById('root').appendChild(dom)
-
-// const textDom = document.createTextNode('')
-// textDom.nodeValue = App.children[0].nodeValue
-// dom.appendChild(textDom)
-
-const App = React.createElement(
-  'div',
-  { id: 'root' },
-  'Hello, World!!!',
-  '你好，世界'
-)
-console.log('🚀 ~ App:', App)
-// render(App, document.querySelector('#root'))
+import App from './App.js'
 
 ReactDom.createRoot(document.querySelector('#root')).render(App)
